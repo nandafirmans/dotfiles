37850b80dc4022dc1dbed7f407fbb8d7dfc4987e
Nanda Firmansyah <nandafirmans@gmail.com>
2023-02-17 01:28:34 +0700

    nvim: bufferline show error info

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -787,6 +787,14 @@ vim.api.nvim_create_autocmd("VimEnter", {
 })
 
 -- BarBar
+require("bufferline").setup({
+  diagnostics = {
+    [vim.diagnostic.severity.ERROR] = { enabled = true, icon = '' },
+    [vim.diagnostic.severity.WARN] = { enabled = false, icon = 'ﬀ' },
+    [vim.diagnostic.severity.INFO] = { enabled = false },
+    [vim.diagnostic.severity.HINT] = { enabled = true },
+  },
+})
 local baseKeymapsOpts = { noremap = true, silent = true }
 
 -- Move to previous/next
