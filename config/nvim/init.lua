4dce2240e3c12ecf6fd0bf497a29cbd84468a0ca
Nanda Firmansyah <nandafirmans@gmail.com>
2023-02-19 23:44:57 +0700

    update nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -346,12 +346,19 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- See `:help lualine.txt`
 require("lualine").setup({
   options = {
-    theme = "sonokai",
+    theme = "auto",
+    -- section_separators = { left = '', right = '' },
     section_separators = { left = "", right = "" },
-    component_separators = { left = "", right = "" },
+    -- component_separators = { left = "", right = "" },
+    component_separators = { left = '', right = '' },
     disabled_filetypes = { "packer", "NvimTree" },
   },
-  sections = { lualine_c = { require("auto-session-library").current_session_name } },
+  sections = {
+    lualine_c = {
+      require("auto-session-library").current_session_name,
+      { 'filename', path = 0, file_status = true },
+    }
+  },
 })
 
 -- Enable Comment.nvim
@@ -424,6 +431,7 @@ vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, { desc
 vim.keymap.set("n", "<leader>fw", require("telescope.builtin").grep_string, { desc = "[F]ind current [W]ord" })
 vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, { desc = "[F]ind by [G]rep" })
 vim.keymap.set("n", "<leader>fd", require("telescope.builtin").diagnostics, { desc = "[F]ind [D]iagnostics" })
+vim.keymap.set("n", "<leader>fc", require("telescope.builtin").commands, { desc = "[F]ind [C]ommands" })
 vim.keymap.set(
   "n",
   "<leader>fb",
